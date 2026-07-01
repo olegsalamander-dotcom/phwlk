31d4e7f482f59f4919e2c5e566180f2ed5211873
olegsalamander-dotcom <oleg.salamander@gmail.com>
2026-07-02 01:12:40 +0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,26 @@
+const CACHE_NAME = 'fotoprogulka-v1.5';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
 self.addEventListener('install', (event) => {
-    self.skipWaiting(); // Принудительно активировать новый воркер
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS);
+        }).then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener('fetch', (event) => {
-    event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+    event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+            return cachedResponse || fetch(event.request).catch(() => caches.match('./index.html'));
+        })
+    );
 });
