eeaa59f0a286b04a957e7281c7c3d7ede14a53fc
olegsalamander-dotcom <oleg.salamander@gmail.com>
2026-07-01 15:20:27 +0400

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,8 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open('phwlk-v1').then((cache) => {
+    return cache.addAll(['./', './index.html', './manifest.json']);
+  }));
+});
+self.addEventListener('fetch', (event) => {
+  event.respondWith(caches.match(event.request).then((response) => response || fetch(event.request)));
+});
